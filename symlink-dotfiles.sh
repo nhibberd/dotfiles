d079788ab893d96006986af125302d0a4ed2e076
nhibberd <nhibberd@gmail.com>
2014-01-25 23:21:56 +1000

    Inluding emacs

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -17,6 +17,12 @@ ln -s dotfiles/x/Xmodmap .Xmodmap
 ln -s dotfiles/x/xsession .xsession
 ln -s dotfiles/x/xinitrc .xinitrc
 
+# emacs
+ln -s dotfiles/emacs/bootstrap/emacs .emacs  
+
+# screen
+#ln -s dotfiles/screen/screenrc .screenrc
+
 # ssh
 [ ! -d $HOME/.ssh ] && mkdir $HOME/.ssh
 )
