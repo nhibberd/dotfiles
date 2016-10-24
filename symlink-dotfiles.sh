b59f35b0fd58d0c2386aadac8cd5691f82f37b55
nhibberd <nick@hibberd.id.au>
2016-10-24 11:04:26 +1100

    git config setup

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -6,7 +6,7 @@ cd $HOME
 # zsh
 ln -s dotfiles/zsh .zsh
 ln -s .zsh/zprofile .zprofile
-ln -s .zsh/zshrc .zshrc 
+ln -s .zsh/zshrc .zshrc
 
 # xmonad
 ln -s dotfiles/xmonad .xmonad
@@ -18,10 +18,14 @@ ln -s dotfiles/x/xsession .xsession
 ln -s dotfiles/x/xinitrc .xinitrc
 
 # emacs
-ln -s dotfiles/emacs/bootstrap/emacs .emacs  
+ln -s dotfiles/emacs/bootstrap/emacs .emacs
 
 # screen
 ln -s dotfiles/screen/screenrc .screenrc
+
+# git
+ln -s dotfiles/gitconfig .gitconfig
+ln -s dotfiles/gitignore .gitignore
 
 # ssh
 [ ! -d $HOME/.ssh ] && mkdir $HOME/.ssh
