b2aafc128e9618e0e4a983c2e3458b1550cffb27
nhibberd <nick@hibberd.id.au>
2017-03-11 11:13:11 +1100

    haskell

--- emacs/src/haskellx.el
+++ emacs/src/haskellx.el
@@ -3,3 +3,19 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
+(defun haskell-mode-on-init ()
+  (set (make-local-variable 'hippie-expand-try-functions-list)
+       '(try-expand-abbrev
+         try-expand-dabbrev))
+  (define-key haskell-mode-map (kbd "<tab>") 'indent-and-complete))
+
+(add-hook 'haskell-mode-hook 'haskell-mode-on-init)
+(add-hook 'haskell-mode-hook 'fuzzit)
+
+(define-abbrev-table 'haskell-mode-abbrev-table ())
+
+(snippet-with-abbrev-table 'haskell-mode-abbrev-table
+  ("f"  . "$>-- FIX ")
+  ("iq"  . "import qualified $.")
+  ("ii"  . "import           $.")
+)
