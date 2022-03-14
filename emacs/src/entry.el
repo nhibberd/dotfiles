fa2ce6e17d98906ce23f7dc53d2fe670f0e18e8b
Nick Hibberd <nhibberd@gmail.com>
2022-03-14 07:47:44 -0700

    projectile

--- emacs/src/entry.el
+++ emacs/src/entry.el
@@ -30,6 +30,13 @@
 (load-library "shellx")
 
 
-(projectile-global-mode)
+;;(projectile-global-mode)
+
+(projectile-mode +1)
+;; Recommended keymap prefix on macOS
+;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;; Recommended keymap prefix on Windows/Linux
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(setq projectile-indexing-method 'native)
 
 (add-to-list 'auto-mode-alist '("\\.bzl\\'" . python-mode))
